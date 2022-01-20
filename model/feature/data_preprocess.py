0cf2831fd673055ccca03abd3430c7c94543a98a
qianxun <ruoyezhuifeng@163.com>
2022-01-20 08:38:22 +0800

    add code

--- model/feature/data_preprocess.py
+++ model/feature/data_preprocess.py
@@ -2,7 +2,8 @@ import sys
 
 sys.path.append('...')
 from data.db.mysql import *
-# from sklearn.feature_selection
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import json
 
@@ -13,27 +14,40 @@ class DataPreprocess(object):
         self.start_date = start_date
         self.end_date = end_date
         self.db = db
-
-
+        self.trans_func_l = []
 
     def read_sql(self, table_name=None, start_date=None, end_date=None):
         sql = f"select * from {table_name} where trade_date between {start_date} and {end_date}"
 
         return sql
 
-    def stocks_preprocess(self,
-                          columns=['ts_code', 'area', 'industry', 'market', 'exchange', 'list_status', 'list_date',
-                                   'delist_date', 'is_hs']):
+    def stocks(self, columns=['ts_code', 'area', 'industry', 'market', 'exchange', 'list_date', 'is_hs']):
+        '''
+        :param columns:
+            20220120：去除 'list_status',因只有一个取值。
+            20220120：去除 'delist_date',因全为空
+        :return:
+        '''
         sql = f"select {','.join(columns)} from stocks_basinfo_cn"
 
         stocks = self.db.read(read_sql=sql)
 
-        stocks['area'].fillna('N', inplace=True)
-        stocks['industry'].fillna('N', inplace=True)
+        stocks.fillna('Nan', inplace=True)
 
+    def cal(self, columns=[]):
 
-    def base_data_combine(self):
-        pass
+        sql = f"select {','.join(columns)} from stocks_basinfo_cn"
+
+    def combine_transform(self):
+        '''
+        整合数据，并进行编码，标准化等特征操作
+        :return:
+        '''
+
+        stocks = 0
+        area_pre = OneHotEncoder().fit(stocks['area'])
+        stocks['area'] = area_pre.tranform(stocks['area'])
+        self.prep_func_l.append({})
 
 
 if __name__ == '__main__':
